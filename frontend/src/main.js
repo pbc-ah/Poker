bfe0fe5c508b1057e716cdd8f396f82cf4619e5b
Agon Hoxha <ah@pbc.group>
2025-12-26 15:38:55 +0100

    minor adjust

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,10 @@ import router from './router'
 import axios from 'axios'
 import signalRService from './services/signalr'
 
-axios.defaults.baseURL = "http://localhost:5267/";
-//axios.defaults.baseURL = "http://192.168.5.58/api/";
+//const baseUrl = "http://localhost:5267/";
+const baseUrl = "http://192.168.5.58:81/";
+
+axios.defaults.baseURL = baseUrl;
 
 const app = createApp(App);
 app.use(router)
@@ -14,7 +16,6 @@ app.use(store);
 app.mount('#app')
 
 // Initialize SignalR connection
-const baseUrl = axios.defaults.baseURL.replace('/api/', '/');
 signalRService.connect(baseUrl).catch(err => {
 	console.error('Failed to establish SignalR connection:', err);
 	// Retry connection after 3 seconds
